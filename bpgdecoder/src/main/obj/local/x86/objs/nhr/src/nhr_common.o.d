9166396d281ef527489d745014bc44571fb78e8d
汪雷 <364664251@qq.com>
2017-02-23 11:36:26 +0800

    删除so文件

--- bpgdecoder/src/main/obj/local/x86/objs/nhr/src/nhr_common.o.d
+++
@@ -1,8 +0,0 @@
-/Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/obj/local/x86/objs/nhr/./src/nhr_common.o: \
- /Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/jni/http/./src/nhr_common.c \
- /Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/jni/http/./src/nhr_common.h \
- /Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/jni/http/./src/../libnhr.h
-
-/Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/jni/http/./src/nhr_common.h:
-
-/Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/jni/http/./src/../libnhr.h:
