5ae744183c86f53c8d5fea2608c6948b20cbfc19
汪雷 <364664251@qq.com>
2017-02-23 11:41:30 +0800

    删除so

--- bpgdecoder/src/main/obj/local/mips/objs/bpg_decoder/decoder.o.d
+++
@@ -1,8 +0,0 @@
-/Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/obj/local/mips/objs/bpg_decoder/decoder.o: \
- /Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/jni/bpg-utils/decoder.c \
- /Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/jni/bpg-utils/decode.h \
- /Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/jni/bpg-utils/../libbpg/libbpg.h
-
-/Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/jni/bpg-utils/decode.h:
-
-/Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/jni/bpg-utils/../libbpg/libbpg.h:
--- bpgdecoder/src/main/obj/local/x86_64/objs/bpg/libavutil/log2_tab.o.d
+++
@@ -1,2 +0,0 @@
-/Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/obj/local/x86_64/objs/bpg/libavutil/log2_tab.o: \
- /Users/wanglei/ray/android/projects/BgpTest/bpgdecoder/src/main/jni/libbpg/libavutil/log2_tab.c
